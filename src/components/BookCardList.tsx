47c68f368411dbee0d13e198488cddf585ca9a2c
Zabrsh <58865909+Zabrsh@users.noreply.github.com>
2023-09-13 01:07:40 +0300

    Update BookCardList.tsx

--- src/components/BookCardList.tsx
+++ src/components/BookCardList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './BookCardList.scss';
 
 const BookCardList: React.FC = () => {
-   // Assuming you have a `books` array in your search state
+ 
  
   return (
     <div className="book-card-list">
